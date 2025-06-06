2cab7532c189302bc747623b1ecdfde7a41f044b
Estefany <167329403+esteffy07@users.noreply.github.com>
2025-06-06 07:57:47 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Site carregado!");
+// Efeito de rolagem suave ao clicar nos links do menu
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const section = document.querySelector(this.getAttribute('href'));
+        if (section) {
+            section.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
 });
 
